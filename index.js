4e11af90b8dfd4f088aea50639b5e987e26d86b2
AlanSAon <alanaontransporte2022@gmail.com>
2022-12-03 14:06:32 -0300

    cambios

--- index.js
+++ index.js
@@ -36,11 +36,13 @@ const buscarTodosProductos = async () => {
       console.log(e.target.id)
 
       const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-const botonesAgregar = document.querySelectorAll(".btn-primary");
+      const botonesAgregar = document.querySelectorAll(".btn-primary");
 
  botonesAgregar.forEach((boton) => {
-   boton.onclick = () => {
-    const producto = buscarTodosProductos.find(
+   boton.onclick = async () => {
+    const productosFetch = await fetch("productos.json");
+    const productosJson = await productosFetch.json();
+    const producto = productosJson.find(
       (prod) => prod.id === parseInt(boton.id)
     );
 
@@ -85,7 +87,7 @@ const buscarProductosPorCategoria = async () => {
   productosFiltrados.forEach((prod) => {
     const { categoria, id, imagen, nombre, precio, talle } = prod;
     divCards.innerHTML += `
-       <div id="${id}" class="card cardProducto">
+      <div id="${id}" class="card cardProducto">
       <img src="${imagen}" class="card-img-top">
       <div class="card-body">
       <h5 class="card-title">${nombre}</h5>
@@ -101,43 +103,3 @@ buscarTodosProductos();
 mostrarCategorias();
 
 botonSeleccionar.onclick = buscarProductosPorCategoria;
-
-//  Carrito de compra
-
-const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-const botonesAgregar = document.querySelectorAll(".btn-primary");
-
- botonesAgregar.forEach((boton) => {
-   boton.onclick = () => {
-    const producto = buscarTodosProductos.find(
-      (prod) => prod.id === parseInt(boton.id)
-    );
-
-    const productoCarrito = {
-      id: producto.id,
-      nombre: producto.nombre,
-      precio: producto.precio,
-      cantidad: 1,
-    };
-    const indexCarrito = carrito.findIndex((prod) => prod.id === producto.id);
-    if (indexCarrito === -1) {
-      carrito.push(productoCarrito);
-    } else {
-      carrito[indexCarrito].cantidad += 1;
-    }
-    saveLocal();
-    console.log(carrito);
-  };
-});
-
-//  Finalizar compra
-
- const botonFinalizar = document.querySelector("#finalizar");
- botonFinalizar.onclick = () => {
-   const totalCompra = carrito.map((prod) => prod.precio * prod.cantidad).reduce((elem1, elem2) => elem1 + elem2,);
-   console.log(totalCompra)
-};
-
- const saveLocal = () => {
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-};
