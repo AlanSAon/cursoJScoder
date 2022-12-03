8dd92a164f35f5db63e999564ff876701192321c
AlanSAon <alanaontransporte2022@gmail.com>
2022-12-02 22:58:04 -0300

    cambios

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-// clase producto
+const divCards = document.querySelector(".cards");
+const lista = document.querySelector("#lista");
+const botonSeleccionar = document.querySelector("#seleccionar");
 
-class Producto {
-  constructor(id, nombre, precio, talle, imagen) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.talle = talle;
-    this.imagen = imagen;
-  }
-}
+const mostrarCategorias = async () => {
+  const categoriasFetch = await fetch("categorias.json");
+  const categoriasJson = await categoriasFetch.json();
+  console.log(categoriasJson);
+  categoriasJson.forEach((cat) => {
+    const option = document.createElement("option");
+    option.innerText = `${cat}`;
+    lista.append(option);
+  });
+};
 
-// Productos
-const producto1 = new Producto(1, "Remera", 700, "L", "img/fotoRemera.jpg");
-const producto2 = new Producto(2, "Bermuda", 600, "L", "img/fotoBermuda.jpg");
-const producto3 = new Producto(3, "Buzo", 800, "L", "img/fotoBuzo.jpg");
-const producto4 = new Producto(4, "Campera", 1000, "L", "img/fotoCampera.jpg");
-const producto5 = new Producto(5, "Pantalon", 850, "L", "img/fotoPantalon.jpg");
-
-// Arreglo de productos
-const productosArray = [producto1, producto2, producto3, producto4, producto5];
-
-// Buscar elementos en el DOM
-
-const divProductos = document.querySelector("#divProductos");
-
-productosArray.forEach((producto) => {
-  divProductos.innerHTML += `
-    <div id="${producto.id}" class="card cardProducto">
-    <img src="${producto.imagen}" class="card-img-top">
-    <div class="card-body">
-    <h5 class="card-title">${producto.nombre}</h5>
-    <p class="card-text">$${producto.precio}</p>
-    <button id="${producto.id}" class="btn btn-primary">AGREGAR AL CARRITO</button>
+const buscarTodosProductos = async () => {
+  const productosFetch = await fetch("productos.json");
+  const productosJson = await productosFetch.json();
+  productosJson.forEach((prod) => {
+    const { categoria, id, imagen, nombre, precio, talle } = prod;
+    divCards.innerHTML += `
+       <div id="${id}" class="card cardProducto">
+      <img src="${imagen}" class="card-img-top">
+      <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+    <p class="card-text">$${precio}</p>
+    <p class="card-text">${talle}</p>
+    <button id="${id}" class="btn btn-primary">AGREGAR AL CARRITO</button>
     </div>
     </div>
     `;
-});
+  });
+  productosJson.forEach((prod) => {
+    const boton = document.getElementById(prod.id)
+    boton.addEventListener("click", (e) => {
+      console.log(e.target.id)
 
-// Carrito de compra
-
-const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+      const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 const botonesAgregar = document.querySelectorAll(".btn-primary");
 
-botonesAgregar.forEach((boton) => {
-  boton.onclick = () => {
-    const producto = productosArray.find(
+ botonesAgregar.forEach((boton) => {
+   boton.onclick = () => {
+    const producto = buscarTodosProductos.find(
       (prod) => prod.id === parseInt(boton.id)
     );
 
@@ -65,17 +61,83 @@ botonesAgregar.forEach((boton) => {
   };
 });
 
-// Finalizar compra
+//  Finalizar compra
 
-const botonFinalizar = document.querySelector("#finalizar");
-botonFinalizar.onclick = () => {
-  const totalCompra = carrito
-    .map((prod) => prod.precio * prod.cantidad)
-    .reduce((elem1, elem2) => elem1 + elem2);
-    console.log(totalCompra);
+ const botonFinalizar = document.querySelector("#finalizar");
+ botonFinalizar.onclick = () => {
+   const totalCompra = carrito.map((prod) => prod.precio * prod.cantidad).reduce((elem1, elem2) => elem1 + elem2,);
+   console.log(totalCompra)
 };
 
-const saveLocal = () => {
+ const saveLocal = () => {
   localStorage.setItem("carrito", JSON.stringify(carrito));
 };
+    })
+  })
+};
 
+const buscarProductosPorCategoria = async () => {
+    divCards.innerHTML = "";
+  const categoriaElegida = lista.value;
+  const productosFetch = await fetch("productos.json");
+  const productosJson = await productosFetch.json();
+  const productosFiltrados = productosJson.filter(prod=>prod.categoria===categoriaElegida)
+  productosFiltrados.forEach((prod) => {
+    const { categoria, id, imagen, nombre, precio, talle } = prod;
+    divCards.innerHTML += `
+       <div id="${id}" class="card cardProducto">
+      <img src="${imagen}" class="card-img-top">
+      <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+    <p class="card-text">$${precio}</p>
+    <p class="card-text">${talle}</p>
+    <button id="${id}" class="btn btn-primary">AGREGAR AL CARRITO</button>
+    </div>
+    </div>
+    `;
+  });
+};
+buscarTodosProductos();
+mostrarCategorias();
+
+botonSeleccionar.onclick = buscarProductosPorCategoria;
+
+//  Carrito de compra
+
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+const botonesAgregar = document.querySelectorAll(".btn-primary");
+
+ botonesAgregar.forEach((boton) => {
+   boton.onclick = () => {
+    const producto = buscarTodosProductos.find(
+      (prod) => prod.id === parseInt(boton.id)
+    );
+
+    const productoCarrito = {
+      id: producto.id,
+      nombre: producto.nombre,
+      precio: producto.precio,
+      cantidad: 1,
+    };
+    const indexCarrito = carrito.findIndex((prod) => prod.id === producto.id);
+    if (indexCarrito === -1) {
+      carrito.push(productoCarrito);
+    } else {
+      carrito[indexCarrito].cantidad += 1;
+    }
+    saveLocal();
+    console.log(carrito);
+  };
+});
+
+//  Finalizar compra
+
+ const botonFinalizar = document.querySelector("#finalizar");
+ botonFinalizar.onclick = () => {
+   const totalCompra = carrito.map((prod) => prod.precio * prod.cantidad).reduce((elem1, elem2) => elem1 + elem2,);
+   console.log(totalCompra)
+};
+
+ const saveLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
